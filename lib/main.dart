5cf78649cf09187c51a59718feee7232c52b2251
pawelpawlik9412 <pawelpawlik9412@gmail.com>
2021-09-21 23:01:19 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WeightTrackerApp());
+}
+
+class WeightTrackerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
